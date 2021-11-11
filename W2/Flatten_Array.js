41f411886b3d9df2be15cdbc3a7ee6ccd1c17a95
JOE Sohn <otterp012@gmail.com>
2021-11-11 12:25:10 +0900

    modification Q1, Q2

--- W2/Flatten_Array.js
+++ W2/Flatten_Array.js
@@ -1,106 +1,41 @@
-
-// 객체분석 정보를 출력
-// 중첩된 깊이를 분석하고 원소의 개수를 출력한다.
-// 대괄호 삭제하는 정규표현식....
-
 const data = "[1,2,[3,4,[5,[6]]]]";
-// 이 string data를 바로 배열화 하는 방법을
-// 도저히 모르겠음..
 
-// 정규식 사용하지 않으면,
-// let newData ="";
-// data.split("").forEach((item) => {
-//     if(item === "[" || item === "]" || item === ",") {
-//         newData += "";
-//     } else newData += item;
-// })
+// 받은 피드백
+/* 숫자가 2자리 이상일 경우를 대비해 ","를 지우는 방식이 아니라,
+  ,를 통해 split 또는 join 하는 방법을 생각해야함.
+   두자리 이상인 경우에 ","를 지우는 방식은 오류가 남! */
+   // -> 해결, 원소의 숫자는 ","의 개수 +1임
 
-// const regExp = /\[.*?\]/g;
 
+// ] 닫는괄호가 처음에 나오는 경우 오류를 리턴해야함. (해결) if문 하나 추가함.
+console.log(data.split(""));
 function printObjInformation(data) {
-  if(isCorrectBracket(data) === "일치합니다."){
-    const dataArray = data.replaceAll("," , "").split("");
+  if(isCorrectBracket(data) === "일치합니다.") {
     let dataDepth = 0;
-    let dataElement = 0;
-    dataArray.forEach((item) => {
-        if(item === "[") dataDepth++;
+    let dataElementNumber = 0;
+    // 괄호 매칭 문제가 없는 경우에만 작동
+    data.split("").forEach((item) => {
+      if(item === "[") dataDepth++;
+      else if(item === ",") dataElementNumber++;
     })
-    dataElement = dataArray.length - dataDepth*2;
-    return `배열의 중첩된 깊이 수준은 ${dataDepth}이며, 총 ${dataElement}의 원소가 포함되어 있습니다.`
-  } else return "올바른 괄호가 아닙니다."
+    return `배열의 중첩된 길이 수준은 ${dataDepth}이며,
+          총 ${dataElementNumber+1}개의 원소가 포함되어 있습니다.`;
+  } else return `올바른 괄호가 아닙니다.`;
 }
-// 사실 이 함수 약간 야매풀이
-// flat 재귀할때마다 count++ 올라가는 풀이 생각해보기
-// + 정규표현식 더 깔끔하게 쓸 수 있는 방법에 대해 고민해보기.
 
 console.log(printObjInformation(data));
-
-// 2-2 stack 자료구조 활용해서.
-// let trueData = "[1,2,[3,4,[5,[6]]]]";
-// let falseData = "[1,2,[3,4,[5,[6]]";
-
-// // 여는괄호가 문제인지, 닫는괄호가 문제인지 확인하는 경우 포함
-
 function isCorrectBracket(data) {
-    const stack = [];
-    data = data.split("");
+  const stack = [];
+  data = data.split(""); 
+  if(data[0] === "]") return "잘못된 괄호입니다." 
+  else {
     data.forEach((item) => {
-        if(item === "[") stack.push(item);
-        else if(item === "]") stack.pop();
+      if(item === "[") stack.push(item);
+      else if(item === "]") stack.pop();
     })
-    if(stack.includes("[")) return "닫는 괄호가 일치하지 않습니다.";
-    else if(stack.includes("]")) return "여는 괄호가 일치하지 않습니다.";
-    else return "일치합니다.";
+  }
+  if(stack.includes("[")) return "닫는 괄호가 일치하지 않습니다.";
+  else if(stack.includes("]")) return "여는 괄호가 일치하지 않습니다.";
+  else return "일치합니다.";
 }
 console.log(isCorrectBracket(data));
-
-// console.log(matchingBracket(falseData));
-// console.log(matchingBracket(trueData));
-
-
-
-
-// 3번
-let stack = [];
-
-let data = "[1,2,[3]]"
-data = data.replaceAll("," , "");
-
-let obj = {};
-
-
-let obj = {
-  "type" : "array",
-  "child": stack,
-}
-let i = 0;
-while(i < data.length) {
-  if(data[i] === "[") {
-    if(stack.length > 0) {
-      console.log(makeObj(stack));
-    }
-    stack.length = 0;
-    i++;
-  } else if(data[i] === "]") break;
-  else {
-    stack.push(printNumInformation(data[i]));
-    console.log(stack);
-    i++;
-  }
-}
-
-console.log(obj);
-function typeCheck(str) {
-  if(typeof(+str) === Number) return "number";
-  else return "array";
-}
-
-function printNumInformation(n) {
-  return {
-    "type": "number",
-    "value": n,
-    "child": [],
-  }
-}
-// stack들을 만드는건 구현했는데,
-// stack들이 각 객체에 자연스럽게 연결을 어떻게 할 수 있는지모르겠음.
