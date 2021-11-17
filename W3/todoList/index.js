68c4d28ad03634d1cfd8518ea0f9aff2c68760f3
JOE Sohn <otterp012@gmail.com>
2021-11-17 19:00:31 +0900

    function refactoring

--- W3/todoList/index.js
+++ W3/todoList/index.js
@@ -2,52 +2,12 @@ const $addButton = document.querySelector('.add-button');
 const $inputSpace = document.querySelector('.input-space');
 const $listContainer = document.querySelector('.list-container');
 
-// $listContainer.appendChild(document.createElement("li"));
-
-// const $list = document.querySelector('li');
-
-// $list.appendChild(document.createElement("input"));
-
-// const $checkBox = document.querySelector("ul input");
-// $checkBox.classList.add("check-box");
-// $checkBox.setAttribute("type", "checkbox");
-
-// $checkBox.appendChild(document.createElement("p"));
-
-function makeNewLists() {
-    const data = $inputSpace.value;
-    const makeLiTag = document.createElement("li");
-    makeLiTag.classList.add("new-list")
-
-    const makeInputTag = document.createElement("input");
-    makeInputTag.setAttribute("type", "checkbox");
-
-    const makePTag = document.createElement("p");
-    const makeDeleteButton = document.createElement("button");
-    makeDeleteButton.innerText = "delete";
-  
-    $listContainer.appendChild(makeLiTag);
-    makeLiTag.appendChild(makeInputTag);
-    makeLiTag.appendChild(makePTag);    
-    makeLiTag.appendChild(makeDeleteButton);
-    makePTag.innerText = data;
-
-    $inputSpace.value = null;
-}
-
-
 $addButton.addEventListener(('click'), () => {
     makeNewLists();
 })
 
-
-console.log($listContainer);
-
-
 $listContainer.addEventListener('click', (event) => {
-    const $checkBox = document.querySelectorAll("li input");
-    console.log(event.target);
-    if(event.target.tagName === "INPUT") {
+    if(event.target.id === "checkbox") {
         event.target.classList.toggle("checked");
 
         if(event.target.classList.contains("checked")) {
@@ -57,69 +17,37 @@ $listContainer.addEventListener('click', (event) => {
         }
     } 
     
-    if(event.target.tagName === "BUTTON") {
+    if(event.target.className === "delete-button") {
         const $list = event.target.parentNode;
-        console.log($list);
-
-        while($list.firstChild) {
-            $list.removeChild($list.firstChild);
-        }
+        removeList($list);
     }
 })
 
+function makeNewLists() {
+    const $data = $inputSpace.value;
+    const makeLiTag = document.createElement("li");
+    const makeInputTag = document.createElement("input");
+    const makePTag = document.createElement("p");
+    const makeDeleteButton = document.createElement("button");
 
-// $listContainer.addEventListener('click', (event) => {
-//     const $checkBox = document.querySelectorAll("li input");
+    $listContainer.appendChild(makeLiTag);
+    makeLiTag.appendChild(makeInputTag);
+    makeLiTag.appendChild(makePTag);    
+    makeLiTag.appendChild(makeDeleteButton);
     
-//     if(event.target.tagName === "INPUT" && $checkBox.checked === true) {
-//         console.log(event.target.parentNode);
-//         event.target.parentNode.classList.add("checked");
-//     } else if (event.target.tagName === "INPUT" && $checkBox.checked === false) {
-//         event.target.parentNode.classList.remove("checked");
-//     }
-// })
+    makeLiTag.classList.add("new-list")
+    makeInputTag.setAttribute("type", "checkbox");
+    makeInputTag.setAttribute("id", "checkbox");
+    makeDeleteButton.setAttribute("class", "delete-button");
 
-// 이벤트 캡쳐링 활용하려고했는데 실패함.
+    makePTag.innerText = $data;
+    makeDeleteButton.innerText = "delete";
 
+    $inputSpace.value = null;
+}
 
-
-// $addButton.addEventListener(('click'), () => {
-
-//     const toDoData = $inputSpace.value;
-
-//     $listContainer.appendChild(document.createElement("li"));
-
-//     const $list = document.querySelector('li');
-    
-//     const makeInputTag = document.createElement("input");
-//     $list.appendChild(makeInputTag);
-    
-//     const $checkBox = document.querySelector("li input");
-//     $checkBox.setAttribute("type", "checkbox");
-    
-//     const makePTag = document.createElement("p");
-//     $list.appendChild(makePTag);
-
-//     const $pText = document.querySelector("li p");
-//     console.log($pText);
-//     $pText.appendChild(document.createTextNode(toDoData));
-//     // 이부분 왜 innerText나 innerHtml 안먹히는지 궁금.
-
-//     const makeDeleteButton = document.createElement("button");
-//     $list.appendChild(makeDeleteButton);
-
-//     $deleteButton = document.querySelector("li button");
-//     $deleteButton.innerText = "delete";
-    
-//     $inputSpace.value = null;
-// })
-
-
-
-
-// var check2_stat = document.getElementById('check2').checked;
-// 체크여부 확인
-
-
-// 돌아는 가는데
-// class로 함수 재구현하기
+function removeList(data) {
+    while(data.firstChild) {
+        data.removeChild(data.firstChild);
+    }
+}
